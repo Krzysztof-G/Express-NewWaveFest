73321daafec4e6070eae1aecae60984e516325a8
Krzysztof Gumuła <k.gumula@icloud.com>
2020-08-03 22:08:21 +0200

    add Heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors')
-// const path = require('path');
+const path = require('path');
 // import routes
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
@@ -10,7 +10,9 @@ const seatsRoutes = require('./routes/seats.routes');
 const app = express();
 
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 app.use(cors());
 
@@ -19,20 +21,19 @@ app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
-// // Serve static files from the React app
-// app.use(express.static(path.join(__dirname, '/client/build')));
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '/client/build/index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found...' });
+  res.status(404).json({
+    message: 'Not found...'
+  });
 })
 
-app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
-}); 
 
-// app.listen(process.env.PORT || 8000, () => {
-//   console.log('Server is running on port: 8000');
-// });
+app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port: 8000');
+});
