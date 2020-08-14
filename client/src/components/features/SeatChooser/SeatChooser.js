11aaae3419c68592385016ee753e36e6cfbc2fda
Krzysztof Gumuła <k.gumula@icloud.com>
2020-08-14 10:28:00 +0200

    fix seats counter

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -56,7 +56,7 @@ class SeatChooser extends React.Component {
         { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].success) && <div className="seats">{[...Array(50)].map((x, i) => prepareSeat(i+1) )}</div>}
         { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].pending) && <Progress animated color="primary" value={50} /> }
         { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].error) && <Alert color="warning">Couldn't load seats...</Alert> }
-        <small id="pickHelp" className="form-text text-muted ml-2"> Free seats: {this.seatsCounter()}/50 </small>
+        <small id="pickHelp" className="form-text text-muted ml-2"> Free seats: {50 - this.seatsCounter()}/50 </small>
       </div>
     )
   };
