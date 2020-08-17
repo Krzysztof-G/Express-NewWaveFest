5f3b33b9ef5c26342ac6c90422d6f8712e95e7c8
Krzysztof Gumuła <k.gumula@icloud.com>
2020-08-17 19:05:56 +0200

    add mongoose connect

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors')
 const path = require('path');
 const socket = require('socket.io');
+const mongoose = require('mongoose');
 // import routes
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
@@ -39,6 +40,13 @@ app.use((req, res) => {
   });
 })
 
+mongoose.connect('mongodb://localhost:27017/NewWaveDB', { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+db.on('error', err => console.log('Error ' + err));
 
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
